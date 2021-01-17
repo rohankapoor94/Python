6536a41843e87e671fa610c9e9be58547b52752e
rohankapoor94 <64392713+rohankapoor94@users.noreply.github.com>
2021-01-17 10:42:25 +0530

    Create Unpacking a Tuple.py

---
+++ Tuple/Unpacking a Tuple.py
@@ -0,0 +1,10 @@
+t=("Apple","Banana","Guava")
+(Red,Yellow,Green)=t
+print(Red)
+print(Yellow)
+print(Green)
+tt=(1,2,3,4,5,6,7,8)
+(Red,Yellow,*Green)=tt#Asterik will help getting multiple value in a single Variable
+print(Green)
+print(Red)
+print(Yellow)
